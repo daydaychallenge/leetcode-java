7e510c18ca0a05f83a70e0c71f8166c8ab1d8739
guozhichao <guozc@allinpay.com>
2020-04-09 15:35:29 +0800

    "init"

---
+++ src/main/java/daydaychange/daily/DayApp.java
@@ -0,0 +1,13 @@
+package daydaychange.daily;
+
+/**
+ * @author nate guo
+ * @version 1.0
+ * @date 2020/4/9 3:23 PM
+ */
+public class DayApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}

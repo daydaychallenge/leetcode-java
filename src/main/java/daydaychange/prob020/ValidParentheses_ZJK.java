4f5337d90dc114897d470c86dc294d894fa6a998
zhangjiangke <zhangjiangke@baidu.com>
2020-04-10 11:08:05 +0800

    valid-parentheses Resolved #2

---
+++ src/main/java/daydaychange/prob020/ValidParentheses_ZJK.java
@@ -0,0 +1,33 @@
+package daydaychange.prob020;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @Author ZhangJk
+ * @Date 2020-04-10 10:35
+ */
+public class ValidParentheses_ZJK {
+
+    public boolean isValid(String s) {
+        Deque<Character> deque = new LinkedList<>();
+        char[] chars = s.toCharArray();
+        for (int i = 0, size = chars.length; i < size; i ++) {
+            char c = chars[i];
+            switch (c) {
+                case '(': deque.push(')'); break;
+                case '{': deque.push('}'); break;
+                case '[': deque.push(']'); break;
+                case ')':
+                case ']':
+                case '}':
+                    if (deque.isEmpty() || c != deque.pop()) {
+                        return false;
+                    }
+                    break;
+                default: return false;
+            }
+        }
+        return deque.isEmpty();
+    }
+}
---
+++ src/test/java/daydaychange/prob020/ValidParentheses_ZJKTest.java
@@ -0,0 +1,26 @@
+package daydaychange.prob020;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Author ZhangJk
+ * @Date 2020-04-10 10:54
+ */
+public class ValidParentheses_ZJKTest {
+
+    ValidParentheses_ZJK valid = new ValidParentheses_ZJK();
+
+    @Test
+    public void validTrue() {
+        String parentheses = "()[({})]{()}";
+        Assertions.assertEquals(Boolean.TRUE, valid.isValid(parentheses));
+    }
+
+    @Test
+    public void validFalse() {
+        String parentheses = "()[({}){()}";
+        Assertions.assertEquals(Boolean.FALSE, valid.isValid(parentheses));
+    }
+
+}
